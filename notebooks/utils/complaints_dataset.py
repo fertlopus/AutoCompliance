f61badd9aa96795af21626d7070b9f138adfdc79
fertlopus <marat.movlamov@icloud.com>
2023-12-11 21:43:13 +0100

    Dataset and Dataloader class for the dataset

---
+++ notebooks/utils/complaints_dataset.py
@@ -0,0 +1,30 @@
+import torch
+from torch.utils.data import DataLoader, Dataset, RandomSampler, SequentialSampler
+
+
+class ComplaintsDataset(Dataset):
+    def __init__(self, texts, labels, tokenizer, max_length):
+        self.texts = texts
+        self.labels = labels
+        self.tokenizer = tokenizer
+        self.max_length = max_length
+
+    def __len__(self):
+        return len(self.texts)
+
+    def __getitem__(self, idx):
+        text = str(self.texts[idx])
+        label = self.labels[idx]
+        encoding = self.tokenizer.encode_plus(
+            text,
+            add_special_tokens = True,
+            max_length = self.max_length,
+            return_token_type_ids = False,
+            padding = "max_length",
+            return_attention_mask = True,
+            return_tensors = "pt",
+            truncation = True)
+        return {
+            "input_ids": encoding["input_ids"].flatten(),
+            "attention_mask": encoding["attention_mask"].flatten(),
+            "labels": torch.tensor(label, dtype = torch.long)}
